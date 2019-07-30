9192f9076f68313f6800fa9796ab9c9030e49507
Martha Hidalgo <martha.hidalgo@sentryhealth.com>
2019-07-30 16:00:37 -0400

    adding comment inside if

--- csv-combiner.php
+++ csv-combiner.php
@@ -11,10 +11,10 @@ foreach ($argv as $index => $file) {
 	if(file_exists($file)){
 		// handle file
 		$handler = fopen($file, 'r');
-		// get headers from the first file only
+		// get headers from the file
 		$header = fgetcsv($handler,0,",");
-		// set headers to output
 		if(!$headers){
+			// set headers to output
 			$h = array_merge($header,['filename']);
 			fputcsv($output, $h);
 			// flag that headers have been set, so we don't try and add them
