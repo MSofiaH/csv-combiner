42ae69c7a82caf183ee6fca3f4bb83596dd0a349
Martha Hidalgo <martha.hidalgo@sentryhealth.com>
2019-07-30 15:38:42 -0400

    commiting csv combiner script

---
+++ csv-combiner.php
@@ -0,0 +1,33 @@
+<?php
+
+$headers = false;
+$output = fopen("php://stdout",'w');
+ini_set("auto_detect_line_endings", true);
+foreach ($argv as $index => $file) {
+	// we skip the first argument, as that is the script we are running
+	if($index == 0)
+		continue;
+	// validate file exists before trying to do anything
+	if(file_exists($file)){
+		// handle file
+		$handler = fopen($file, 'r');
+		// get headers from the first file only
+		$header = fgetcsv($handler,0,",");
+		// set headers to output
+		if(!$headers){
+			$h = array_merge($header,['filename']);
+			fputcsv($output, $h);
+			// flag that headers have been set, so we don't try and add them
+			// when we read the next file
+			$headers = true;
+		}
+		// iterate through the rest of the rows in file
+		while ($row = fgetcsv($handler,0,",")){
+			$r = array_merge($row,[basename($file)]);
+			fputcsv($output, $r);
+		}
+		fclose($handler);	
+	}
+}
+fclose($output);
+?>
